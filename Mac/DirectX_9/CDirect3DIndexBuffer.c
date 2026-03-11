d2d2c49184c6ffa7d91359634207894c46c3e09b
decomp <decomp@local>
2026-03-11 17:54:54 +0100

    Add CMemoryBuffer support and refactor CDirect3DIndexBuffer impl

--- Mac/DirectX_9/CDirect3DIndexBuffer.c
+++ Mac/DirectX_9/CDirect3DIndexBuffer.c
@@ -9,9 +9,37 @@
  *   #include "Mac/DirectX 9/MacOpenGLUtils.h"
  */
 
+typedef struct {
+    int vptr;
+    byte *allocation;
+    byte *data;
+    UINT32 length;
+    int freedLater;
+} CMemoryBufferImpl;
+
+typedef struct {
+    void **vtable;
+    ULONG refCount;
+    UINT32 lengthBytes;
+    CMemoryBufferImpl memory;
+    UINT32 indexSizeBytes;
+    DWORD usage;
+    byte *lockPtr;
+    UINT32 lockSize;
+    unsigned char isLocked;
+} CDirect3DIndexBufferImpl;
+
+extern void *vtbl_CDirect3DIndexBuffer[];
+void __ZdlPv(void *ptr);
+void CMemoryBuffer_CMemoryBuffer(const CMemoryBuffer * _this, UINT32 Length);
+void CMemoryBuffer_Recreate(const CMemoryBuffer * _this);
+void ZN13CMemoryBufferD1Ev(const CMemoryBuffer * _this); /* CMemoryBuffer_~CMemoryBuffer */
+void CMemoryBuffer_FreeLater(const CMemoryBuffer * _this, UINT32 Frames);
+UINT32 MacOpenGLUtils_GetFormatSizeInBits(const D3DFORMAT *f);
+
 ULONG CDirect3DIndexBuffer_AddRef(const CDirect3DIndexBuffer * _this);
-void ZN20CDirect3DIndexBufferD1Ev(void); /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
-void ZN20CDirect3DIndexBufferD0Ev(void); /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
+void ZN20CDirect3DIndexBufferD1Ev(const CDirect3DIndexBuffer * _this); /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
+void ZN20CDirect3DIndexBufferD0Ev(const CDirect3DIndexBuffer * _this); /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
 HRESULT CDirect3DIndexBuffer_QueryInterface(const CDirect3DIndexBuffer * _this, const IID *iid, LPVOID *ppvObj);
 ULONG CDirect3DIndexBuffer_Release(const CDirect3DIndexBuffer * _this);
 HRESULT CDirect3DIndexBuffer_Unlock(const CDirect3DIndexBuffer * _this);
@@ -27,339 +55,166 @@ J_COLOR_SPACE CDirect3DIndexBuffer_PreLoad(const CDirect3DIndexBuffer * _this);
 D3DRESOURCETYPE CDirect3DIndexBuffer_GetType(const CDirect3DIndexBuffer * _this);
 HRESULT CDirect3DIndexBuffer_GetDesc(const CDirect3DIndexBuffer * _this, D3DINDEXBUFFER_DESC *pDesc);
 
-/* line 66 */
-__attribute__((naked))
 ULONG CDirect3DIndexBuffer_AddRef(const CDirect3DIndexBuffer * _this)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 66 */
-        "movl %esp, %ebp\n"
-        "movl 8(%ebp), %edx\n" /* this */
-        "movl 4(%edx), %eax\n" /* line 68 */
-        "addl $1, %eax\n"
-        "movl %eax, 4(%edx)\n"
-        "popl %ebp\n" /* line 69 */
-        "retl\n"
-    );
+    CDirect3DIndexBufferImpl *indexBuffer;
+
+    indexBuffer = (CDirect3DIndexBufferImpl *)_this;
+    ++indexBuffer->refCount;
+    return indexBuffer->refCount;
 }
 
-/* line 44 */
-__attribute__((naked))
-void ZN20CDirect3DIndexBufferD1Ev(void) /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
+void ZN20CDirect3DIndexBufferD1Ev(const CDirect3DIndexBuffer * _this) /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 44 */
-        "movl %esp, %ebp\n"
-        "movl 8(%ebp), %eax\n" /* this */
-        "movl $vtbl_CDirect3DIndexBuffer, (%eax)\n"
-        "addl $0xc, %eax\n" /* line 47 */
-        "movl %eax, 8(%ebp)\n" /* this */
-        "popl %ebp\n"
-        "jmp ZN13CMemoryBufferD1Ev\n"
-    );
+    CDirect3DIndexBufferImpl *indexBuffer;
+
+    indexBuffer = (CDirect3DIndexBufferImpl *)_this;
+    indexBuffer->vtable = vtbl_CDirect3DIndexBuffer;
+    ZN13CMemoryBufferD1Ev((const CMemoryBuffer *)((const byte *)_this + 0xc));
 }
 
-/* line 44 */
-__attribute__((naked))
-void ZN20CDirect3DIndexBufferD0Ev(void) /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
+void ZN20CDirect3DIndexBufferD0Ev(const CDirect3DIndexBuffer * _this) /* CDirect3DIndexBuffer_~CDirect3DIndexBuffer */
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 44 */
-        "movl %esp, %ebp\n"
-        "pushl %ebx\n"
-        "subl $0x14, %esp\n"
-        "movl 8(%ebp), %ebx\n" /* this */
-        "movl $vtbl_CDirect3DIndexBuffer, (%ebx)\n" /* this */
-        "leal 0xc(%ebx), %eax\n" /* line 47 | this */
-        "movl %eax, (%esp)\n"
-        "calll ZN13CMemoryBufferD1Ev\n"
-        "movl %ebx, 8(%ebp)\n" /* this */
-        "addl $0x14, %esp\n"
-        "popl %ebx\n"
-        "popl %ebp\n"
-        "jmp __ZdlPv\n"
-    );
+    ZN20CDirect3DIndexBufferD1Ev(_this);
+    __ZdlPv((void *)_this);
 }
 
-/* line 55 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_QueryInterface(const CDirect3DIndexBuffer * _this, const IID *iid, LPVOID *ppvObj)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 55 */
-        "movl %esp, %ebp\n"
-        "subl $0x18, %esp\n"
-        "movl 8(%ebp), %eax\n" /* this */
-        "movl 0x10(%ebp), %edx\n" /* line 57 | ppvObj */
-        "movl %eax, (%edx)\n"
-        "movl (%eax), %edx\n" /* line 58 */
-        "movl %eax, (%esp)\n"
-        "calll *4(%edx)\n"
-        "xorl %eax, %eax\n" /* line 61 */
-        "leave\n"
-        "retl\n"
-    );
+    (void)iid;
+
+    *ppvObj = (LPVOID)_this;
+    CDirect3DIndexBuffer_AddRef(_this);
+    return 0;
 }
 
-/* line 74 */
-__attribute__((naked))
 ULONG CDirect3DIndexBuffer_Release(const CDirect3DIndexBuffer * _this)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 74 */
-        "movl %esp, %ebp\n"
-        "pushl %ebx\n"
-        "subl $0x14, %esp\n"
-        "movl 8(%ebp), %edx\n" /* this */
-        "movl 4(%edx), %ebx\n" /* line 76 */
-        "subl $1, %ebx\n"
-        "movl %ebx, 4(%edx)\n"
-        "testl %ebx, %ebx\n" /* line 78 */
-        "jne .Lf1be54_0001be73\n"
-        "movl (%edx), %eax\n" /* line 80 */
-        "movl %edx, (%esp)\n"
-        "calll *0x3c(%eax)\n"
-        ".Lf1be54_0001be73:\n"
-        "movl %ebx, %eax\n" /* line 84 */
-        "addl $0x14, %esp\n"
-        "popl %ebx\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    CDirect3DIndexBufferImpl *indexBuffer;
+    ULONG refCount;
+
+    indexBuffer = (CDirect3DIndexBufferImpl *)_this;
+    refCount = --indexBuffer->refCount;
+    if (!refCount) {
+        ZN20CDirect3DIndexBufferD0Ev(_this);
+    }
+
+    return refCount;
 }
 
-/* line 168 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_Unlock(const CDirect3DIndexBuffer * _this)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 168 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n" /* line 171 */
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    return 0;
 }
 
-/* line 94 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_Lock(const CDirect3DIndexBuffer * _this, UINT OffsetToLock, UINT SizeToLock, J_COLOR_SPACE * *ppbDat, DWORD Flags)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 94 */
-        "movl %esp, %ebp\n"
-        "pushl %edi\n"
-        "pushl %esi\n"
-        "pushl %ebx\n"
-        "subl $0x1c, %esp\n"
-        "movl 8(%ebp), %edi\n" /* this */
-        "movl 0xc(%ebp), %esi\n" /* OffsetToLock */
-        "movl 0x10(%ebp), %eax\n" /* SizeToLock, ppbDat */
-        "movb $1, 0x30(%edi)\n" /* line 64 */
-        "testl %eax, %eax\n" /* line 109 */
-        "jne .Lf1be84_0001bea1\n"
-        "movl 8(%edi), %eax\n" /* this */
-        ".Lf1be84_0001bea1:\n"
-        "movl %eax, 0x2c(%edi)\n" /* this */
-        "testb $2, 0x25(%edi)\n" /* line 115 | this */
-        "je .Lf1be84_0001beb3\n"
-        "testl $0x2000, 0x18(%ebp)\n" /* line 119 | Flags */
-        "jne .Lf1be84_0001beca\n"
-        ".Lf1be84_0001beb3:\n"
-        "addl 0x14(%edi), %esi\n" /* line 157 | this, OffsetToLock */
-        "movl %esi, 0x28(%edi)\n" /* OffsetToLock, this */
-        "movl %esi, %edx\n" /* OffsetToLock */
-        "movl 0x14(%ebp), %eax\n" /* line 160 | ppbDat */
-        "movl %edx, (%eax)\n"
-        "xorl %eax, %eax\n" /* line 163 */
-        "addl $0x1c, %esp\n"
-        "popl %ebx\n"
-        "popl %esi\n"
-        "popl %edi\n"
-        "popl %ebp\n"
-        "retl\n"
-        ".Lf1be84_0001beca:\n"
-        "leal 0xc(%edi), %ebx\n" /* line 129 | this */
-        "movl $2, 4(%esp)\n"
-        "movl %ebx, (%esp)\n"
-        "calll CMemoryBuffer_FreeLater\n"
-        "movl %ebx, (%esp)\n" /* line 130 */
-        "calll CMemoryBuffer_Recreate\n"
-        "addl 8(%ebx), %esi\n" /* line 132 | OffsetToLock */
-        "movl %esi, 0x28(%edi)\n" /* OffsetToLock, this */
-        "movl %esi, %edx\n" /* OffsetToLock */
-        "movl 0x14(%ebp), %eax\n" /* line 160 | ppbDat */
-        "movl %edx, (%eax)\n"
-        "xorl %eax, %eax\n" /* line 163 */
-        "addl $0x1c, %esp\n"
-        "popl %ebx\n"
-        "popl %esi\n"
-        "popl %edi\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    CDirect3DIndexBufferImpl *indexBuffer;
+
+    indexBuffer = (CDirect3DIndexBufferImpl *)_this;
+    indexBuffer->isLocked = 1;
+    indexBuffer->lockSize = SizeToLock ? SizeToLock : indexBuffer->lengthBytes;
+
+    if ((indexBuffer->usage & 0x200) && (Flags & 0x2000)) {
+        CMemoryBuffer_FreeLater((const CMemoryBuffer *)&indexBuffer->memory, 2);
+        CMemoryBuffer_Recreate((const CMemoryBuffer *)&indexBuffer->memory);
+    }
+
+    indexBuffer->lockPtr = indexBuffer->memory.data + OffsetToLock;
+    *ppbDat = (J_COLOR_SPACE *)indexBuffer->lockPtr;
+    return 0;
 }
 
-/* line 25 */
-__attribute__((naked))
 J_COLOR_SPACE CDirect3DIndexBuffer_CDirect3DIndexBuffer(const CDirect3DIndexBuffer * _this, UINT32 Length, D3DFORMAT Format, DWORD Usage, D3DPOOL Pool)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 25 */
-        "movl %esp, %ebp\n"
-        "pushl %esi\n"
-        "pushl %ebx\n"
-        "subl $0x10, %esp\n"
-        "movl 8(%ebp), %ebx\n" /* this */
-        "movl $vtbl_CDirect3DIndexBuffer, (%ebx)\n" /* line 33 | this */
-        "movl $0, 4(%ebx)\n" /* this */
-        "movl 0xc(%ebp), %eax\n" /* line 147 | Length */
-        "addl $3, %eax\n"
-        "andl $0xfffffffc, %eax\n"
-        "movl %eax, 8(%ebx)\n" /* line 33 | this */
-        "leal 0xc(%ebx), %esi\n" /* this */
-        "movl %eax, 4(%esp)\n"
-        "movl %esi, (%esp)\n"
-        "calll CMemoryBuffer_CMemoryBuffer\n"
-        "leal 0x10(%ebp), %eax\n" /* Format */
-        "movl %eax, (%esp)\n"
-        "calll MacOpenGLUtils_GetFormatSizeInBits\n"
-        "shrl $3, %eax\n"
-        "movl %eax, 0x20(%ebx)\n" /* this */
-        "movl 0x14(%ebp), %eax\n" /* Usage */
-        "movl %eax, 0x24(%ebx)\n" /* this */
-        "movl $0, 0x28(%ebx)\n" /* this */
-        "movl $0, 0x2c(%ebx)\n" /* this */
-        "movb $0, 0x30(%ebx)\n" /* this */
-        "addl $1, 4(%ebx)\n" /* line 68 | this */
-        "addl $0x10, %esp\n" /* line 40 */
-        "popl %ebx\n"
-        "popl %esi\n"
-        "popl %ebp\n"
-        "retl\n"
-        "movl %eax, %ebx\n" /* this */
-        "movl %esi, (%esp)\n"
-        "calll ZN13CMemoryBufferD1Ev\n"
-        "movl %ebx, (%esp)\n" /* this */
-        "calll __Unwind_Resume\n"
-    );
+    CDirect3DIndexBufferImpl *indexBuffer;
+
+    (void)Pool;
+
+    indexBuffer = (CDirect3DIndexBufferImpl *)_this;
+    indexBuffer->vtable = vtbl_CDirect3DIndexBuffer;
+    indexBuffer->refCount = 0;
+    indexBuffer->lengthBytes = (Length + 3) & ~3U;
+
+    CMemoryBuffer_CMemoryBuffer((const CMemoryBuffer *)&indexBuffer->memory, indexBuffer->lengthBytes);
+
+    indexBuffer->indexSizeBytes = MacOpenGLUtils_GetFormatSizeInBits(&Format) >> 3;
+    indexBuffer->usage = Usage;
+    indexBuffer->lockPtr = NULL;
+    indexBuffer->lockSize = 0;
+    indexBuffer->isLocked = 0;
+    ++indexBuffer->refCount;
+    return 0;
 }
 
-/* overload skip: CDirect3DIndexBuffer_CDirect3DIndexBuffer (0x1bf76) */
-
-/* line 51 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_GetDevice(const CDirect3DIndexBuffer * _this, IDirect3DDevice9 * *ppDevice)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 51 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    (void)ppDevice;
+    return 0;
 }
 
-/* line 52 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_SetPrivateData(const CDirect3DIndexBuffer * _this, const GUID *refguid, const J_COLOR_SPACE *pData, DWORD SizeOfData, DWORD Flags)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 52 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    (void)refguid;
+    (void)pData;
+    (void)SizeOfData;
+    (void)Flags;
+    return 0;
 }
 
-/* line 53 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_GetPrivateData(const CDirect3DIndexBuffer * _this, const GUID *refguid, J_COLOR_SPACE *pData, DWORD *pSizeOfData)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 53 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    (void)refguid;
+    (void)pData;
+    (void)pSizeOfData;
+    return 0;
 }
 
-/* line 54 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_FreePrivateData(const CDirect3DIndexBuffer * _this, const GUID *refguid)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 54 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    (void)refguid;
+    return 0;
 }
 
-/* line 55 */
-__attribute__((naked))
 DWORD CDirect3DIndexBuffer_SetPriority(const CDirect3DIndexBuffer * _this, DWORD PriorityNew)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 55 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    (void)PriorityNew;
+    return 0;
 }
 
-/* line 56 */
-__attribute__((naked))
 DWORD CDirect3DIndexBuffer_GetPriority(const CDirect3DIndexBuffer * _this)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 56 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    return 0;
 }
 
-/* line 57 */
-__attribute__((naked))
 J_COLOR_SPACE CDirect3DIndexBuffer_PreLoad(const CDirect3DIndexBuffer * _this)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 57 */
-        "movl %esp, %ebp\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    return 0;
 }
 
-/* line 58 */
-__attribute__((naked))
 D3DRESOURCETYPE CDirect3DIndexBuffer_GetType(const CDirect3DIndexBuffer * _this)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 58 */
-        "movl %esp, %ebp\n"
-        "movl $7, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
+    (void)_this;
+    return D3DRTYPE_INDEXBUFFER;
 }
 
-/* line 61 */
-__attribute__((naked))
 HRESULT CDirect3DIndexBuffer_GetDesc(const CDirect3DIndexBuffer * _this, D3DINDEXBUFFER_DESC *pDesc)
 {
-    __asm__ __volatile__ (
-        "pushl %ebp\n" /* line 61 */
-        "movl %esp, %ebp\n"
-        "xorl %eax, %eax\n"
-        "popl %ebp\n"
-        "retl\n"
-    );
-}
+    CDirect3DIndexBufferImpl *indexBuffer;
 
+    indexBuffer = (CDirect3DIndexBufferImpl *)_this;
+    pDesc->Format = indexBuffer->indexSizeBytes == 2 ? D3DFMT_INDEX16 : D3DFMT_INDEX32;
+    pDesc->Type = D3DRTYPE_INDEXBUFFER;
+    pDesc->Usage = indexBuffer->usage;
+    pDesc->Pool = 0;
+    pDesc->Size = indexBuffer->lengthBytes;
+    return 0;
+}
